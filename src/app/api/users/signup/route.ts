754814c3cbf8592ebb42eba92d287be4fc046385
Subrat <chaudharysubrat78@outlook.com>
2024-10-23 22:18:39 +0530

    mailer written

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -10,17 +10,35 @@ export async function POST(request: NextRequest) {
 		const body = await request.json();
 		const { userName, email, password } = body;
 
-        if (!userName || !email || !password) {
-            return NextResponse.json({ error: "All fields are required" }, { status: 400 });
-        }
-        const user = await User.findOne({ email });
+		if (!userName || !email || !password) {
+			return NextResponse.json(
+				{ error: "All fields are required" },
+				{ status: 400 }
+			);
+		}
+		const user = await User.findOne({ email });
 
-        if (user) {
-            return NextResponse.json({ error: "User already exists" }, { status: 400 });
-        }
-		const createdUser = await User.create({ userName, email, password });
-        sendMail({email, mailType: "verifyEmail", userId: user._id})
-		return NextResponse.json({ user });
+		if (user) {
+			return NextResponse.json(
+				{ error: "User already exists" },
+				{ status: 400 }
+			);
+		}
+
+		const salts = await bcryptjs.genSalt(10);
+
+		const hashedPassword = await bcryptjs.hash(password, salts);
+		const createdUser = new User({ userName, email, password: hashedPassword });
+		const savedUser = await createdUser.save();
+		await sendMail({ email, mailType: "verifyEmail", userId: savedUser._id });
+		return NextResponse.json(
+			{
+				message: "User created successfully",
+				user: savedUser,
+				success: true,
+			},
+			{ status: 200 }
+		);
 	} catch (error: any) {
 		return NextResponse.json({ error: error.message }, { status: 500 });
 	}
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,27 +1,63 @@
+import User from "@/models/userModel";
 import nodemailer from "nodemailer";
-
-export const sendMail = async ({email, mailType, userId}:any) => {
+import { v4 as uuid } from "uuid";
+export const sendMail = async ({ email, mailType, userId }: any) => {
 	try {
-        const transporter = nodemailer.createTransport({
-        		host: "smtp.ethereal.email",
-        		port: 587,
-        		secure: false, // true for port 465, false for other ports
-        		auth: {
-        			user: "maddison53@ethereal.email",
-        			pass: "jn7jnAPss4f63QBp6D",
-        		},
-        	});
-        
-        	const mailOptions = {
-        		from: "subrat@chaudhary.ai",
-        		to: email,
-        		subject: mailType === "forgotPassword" ? "Reset Your Password" : "Verify Your Email",
-        		html: "<b>Hello world?</b>",
-        	};
-        
-            const mailResponse = await transporter.sendMail(mailOptions);
-            return mailResponse
-    } catch (error:any) {
-        throw new Error(error.message);   
-    }
+		const token = uuid();
+		if (mailType === "verifyEmail") {
+			await User.findByIdAndUpdate(userId, {
+				verifyToken: token,
+				verifyTokenExpiry: Date.now() + 3600000,
+			});
+		}
+		if (mailType === "forgotPassword") {
+			await User.findByIdAndUpdate(userId, {
+				forgotPasswordToken: token,
+				forgotPasswordExpiry: Date.now() + 3600000,
+			});
+		}
+		var transport = nodemailer.createTransport({
+			host: "sandbox.smtp.mailtrap.io",
+			port: 2525,
+			auth: {
+				user: process.env.MAILTRAP_USER,
+				pass: process.env.MAILTRAP_PASS,
+			},
+		});
+
+		const mailOptions = {
+			from: "subrat@chaudhary.ai",
+			to: email,
+			subject:
+				mailType === "forgotPassword"
+					? "Reset Your Password"
+					: "Verify Your Email",
+			html:
+				mailType === "forgotPassword"
+					? `<div>
+					<h2>Password Reset Request</h2>
+					<p>Hello User,</p>
+					<p>We received a request to reset your password. If you did not request this, please ignore this email.</p>
+					<p>To reset your password, click the link below:</p>
+					<p><a href=${process.env.DOMAIN}/forgotPassword?token=${token} class="button">Reset Password</a></p>
+					<p>Thank you!</p>
+					<p>Best regards,<br>Your Company Name</p>
+				</div>`
+					: `<div>
+					<h1>Please Verify Your Email Address</h1>
+					<p>Dear New User,</p>
+					<p>Thank you for signing up! To complete your registration, please verify your email address by clicking the link below:</p>
+					<a href=${process.env.DOMAIN}/verifyEmail?token=${token}>Verify Email Address</a>
+					<p>or copy the link below into your browser</p>
+					<p>${process.env.DOMAIN}/verifyEmail?token=${token}</p>
+					<p>If you did not create an account, no further action is required.</p>
+					<p>Best regards,<br>XYZ Inc.</p>
+   				 </div>`,
+		};
+
+		const mailResponse = await transport.sendMail(mailOptions);
+		return mailResponse;
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
 };
