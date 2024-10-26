5b65f171f685bee914cbb30600f09f4cf7de84bd
Subrat <chaudharysubrat78@outlook.com>
2024-10-26 19:34:15 +0530

    logout route fix

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -2,7 +2,7 @@ import { dbConnection } from "@/dbConnection/dbConnection";
 import { NextRequest, NextResponse } from "next/server";
 
 dbConnection();
-async function POST() {
+async function GET() {
 	const response = NextResponse.json(
 		{ message: "Logout Success", success: true },
 		{ status: 200 }
