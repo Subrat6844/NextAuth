b24d6d56ac79eafe437153268b2f5e9101838399
Subrat <chaudharysubrat78@outlook.com>
2024-10-26 19:56:05 +0530

    me route added

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,19 @@
+import { dbConnection } from "@/dbConnection/dbConnection";
+import { NextRequest, NextResponse } from "next/server";
+import jwt from "jsonwebtoken";
+import User from "@/models/userModel";
+dbConnection();
+
+async function POST(request: NextRequest) {
+	const token = request.cookies.get("auth")?.value || "";
+	const decodedToken: any = jwt.verify(token, process.env.JWT_SECRET!);
+
+	const user = User.findById(decodedToken.id).select("-password");
+	return NextResponse.json(
+		{
+			message: "Current User Found",
+			data: user,
+		},
+		{ status: 200 }
+	);
+}
